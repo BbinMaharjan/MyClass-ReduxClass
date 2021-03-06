39cf539cb3767f0f27e70370574299f872a50692
BbinMaharjan <bbinmaharjan@gmail.com>
2021-03-06 20:56:46 +0545

    initial setup

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const HomeScreen = (props) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Page</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default HomeScreen;
